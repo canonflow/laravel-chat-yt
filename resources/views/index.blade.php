baf8cec68a3297cedecf1de26ad28baa52552b10
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2023-08-10 14:00:12 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Chat</title>
+
+    <link rel="stylesheet" href="./css/app.css">
+</head>
+<body>
+    <div class="app">
+        <header>
+            <h1>Let's Talk!</h1>
+            <input type="text" name="username" id="username" placeholder="Please enter a username..." />
+        </header>
+        <div id="messages"></div>
+
+        <form action="" id="message_form">
+            <input type="text" name="message" id="message_input" placeholder="Write a message..." />
+            <button type="submit" id="message_send">Send Message</button>
+        </form>
+    </div>
+    <script src="./js/app.js"></script>
+</body>
+</html>
